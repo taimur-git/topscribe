a0a465beb5b90bf498d5faf91f0edf36c46a1c63
Reyad02 <almomenreyad@Gmail.com>
2023-03-27 10:07:05 +0600

    moved landing page to partial

--- index.php
+++ index.php
@@ -12,30 +12,10 @@ if(isset($_SESSION['name'])) {
     showAllWriting($conn);
 }else{
     //landing page
-    
-    ?>
-    <body style="overflow-y:hidden">
-    <div class="scroll-container">
-        <div class="scroll-area">
-            <p class="landing_heads">
-                <br><br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Where writing
-                <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;becomes your Passion</p>
-            <p class="landing_subs">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hone your writing skills to<br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;perfection by regular practice,<br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;participating in contest and<br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;publishing your best pieces
-            </p>    
-        </div>
-        <div class="scroll-area">2</div>
-        <div class="scroll-area">3</div>
-        <div class="scroll-area">4</div>
-    </div>    
-         <?php include('partial/footer.php');?>
-    <?php
-   
+    include('partial/landing.php');
 }
+      include('partial/footer.php');?>
+    
 
 
-?>
 
---
+++ partial/landing.php
@@ -0,0 +1,17 @@
+<body style="overflow-y:hidden">
+    <div class="scroll-container">
+        <div class="scroll-area">
+            <p class="landing_heads">
+                <br><br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Where writing
+                <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;becomes your Passion</p>
+            <p class="landing_subs">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hone your writing skills to<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;perfection by regular practice,<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;participating in contest and<br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;publishing your best pieces
+            </p>    
+        </div>
+        <div class="scroll-area">2</div>
+        <div class="scroll-area">3</div>
+        <div class="scroll-area">4</div>
+    </div>   
